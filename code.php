30c7311ff574c26f12a07e60f6db93426956d15c
Evgeniy Tokmakov <tokmakov.e@mail.ru>
2018-05-30 09:39:07 +0300

    Незначительные изменения

--- code.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-$code = file_get_contents('code.txt');
-require 'app/include/Highlight.php';
-$hl = new Highlight();
-$res = $hl->highlightXML($code);
-echo $res;
